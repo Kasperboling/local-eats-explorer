5e02cf643faa653edcc3c6e9e95a5b01c79b615d
kasper <kasper.boling@gmail.com>
2023-08-24 09:09:46 +0300

    Teaser beggining

--- web/app/cache/acorn/framework/views/fe94ae094d14c8a6d178100999c7482f6c744f2a.php
+++ web/app/cache/acorn/framework/views/fe94ae094d14c8a6d178100999c7482f6c744f2a.php
@@ -13,38 +13,13 @@
 
     </h3>
 
-    
-                <?php echo render_block([
-                    'blockName' => 'core/post-date',
-                    'attrs' => array_merge(
-                        ['name' => 'core/post-date'],
-                        [
-      'format' => 'd.m.Y',
-      'className' => 'teaser__date'
-    ]
-                    ),
-                ]); ?>
-            
-
     <?php if($excerpt = get_the_excerpt()): ?>
       <p class="teaser__description"><?php echo $excerpt; ?></p>
-    <?php endif; ?>
-
-    <?php if($categories = get_the_terms(get_post(), 'category')): ?>
-      <div class="teaser__tags">
-        <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $term): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-          <a class="teaser__tag" href="<?php echo e(get_term_link($term)); ?>">
-            <?php echo esc_html($term->name); ?>
-
-          </a>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-      </div>
     <?php endif; ?>
 
     <p>
       <a class="teaser__readmore teaser__link" href="<?php echo e(get_permalink()); ?>">
         <span><?php echo e(__('Read article', 'gds')); ?></span>
-        <i class="fa-solid fa-chevrons-right fa-2xs"></i>
       </a>
     </p>
   </div>
--- web/app/themes/gds/resources/views/teasers/teaser.blade.php
+++ web/app/themes/gds/resources/views/teasers/teaser.blade.php
@@ -11,29 +11,13 @@
       {!! esc_html(get_the_title()) !!}
     </h3>
 
-    @block('core/post-date', [
-      'format' => 'd.m.Y',
-      'className' => 'teaser__date'
-    ])
-
     @if ($excerpt = get_the_excerpt())
       <p class="teaser__description">{!! $excerpt !!}</p>
-    @endif
-
-    @if ($categories = get_the_terms(get_post(), 'category'))
-      <div class="teaser__tags">
-        @foreach ($categories as $term)
-          <a class="teaser__tag" href="{{ get_term_link($term) }}">
-            {!! esc_html($term->name) !!}
-          </a>
-        @endforeach
-      </div>
     @endif
 
     <p>
       <a class="teaser__readmore teaser__link" href="{{ get_permalink() }}">
         <span>{{ __('Read article', 'gds') }}</span>
-        <i class="fa-solid fa-chevrons-right fa-2xs"></i>
       </a>
     </p>
   </div>
